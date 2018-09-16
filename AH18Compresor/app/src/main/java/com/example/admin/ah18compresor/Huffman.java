2588664af33b7716e5ea024e937b1e85aa7314c8
JoshHC <davidhigueros@gmail.com>
2018-09-15 22:10:45 -0600

    Explorador de Archivos
    Implementacion del Explorador de Archivos

--- AH18Compresor/app/src/main/java/com/example/admin/ah18compresor/Huffman.java
+++ AH18Compresor/app/src/main/java/com/example/admin/ah18compresor/Huffman.java
@@ -7,15 +7,106 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.view.Window;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.List;
 
 
-public class Huffman extends Fragment {
+public class Huffman extends Fragment implements OnItemClickListener {
+
+    private List<String> NombresArchivos;
+    private List<String> RutasArchivos;
+    private ArrayAdapter<String> Adaptador;
+    private String DirectorioRaiz;
+    private TextView CarpetaActual;
+    ListView Lista;
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_huffman, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_huffman, container, false);
+
+        CarpetaActual = (TextView) view.findViewById(R.id.RutaActual);
+        Lista = (ListView) view.findViewById(R.id.Lista);
+
+        DirectorioRaiz = Environment.getExternalStorageDirectory().getPath();
+        Lista.setOnItemClickListener(this);
+        VerDirectorio(DirectorioRaiz);
+
+        return view;
     }
 
+    private void VerDirectorio (String rutadirectorio)
+    {
+        NombresArchivos = new ArrayList<String>();
+        RutasArchivos = new ArrayList<String>();
+        int count = 0;
+        File directorioactual = new File(rutadirectorio);
+        File[] ListadeArchivos = directorioactual.listFiles();
+
+        if (!rutadirectorio.equals(DirectorioRaiz))
+        {
+            NombresArchivos.add("../");
+            RutasArchivos.add(directorioactual.getParent());
+            count = 1;
+        }
+
+        for (File archivo:ListadeArchivos){
+            RutasArchivos.add(archivo.getPath());
+        }
+
+        Collections.sort(RutasArchivos,String.CASE_INSENSITIVE_ORDER);
+
+        for (int i = count; i<RutasArchivos.size(); i++){
+            File archivo = new File (RutasArchivos.get(i));
+
+            if(archivo.isFile())
+            {
+                NombresArchivos.add(archivo.getName());
+            }
+            else
+            {
+                NombresArchivos.add("/" + archivo.getName());
+            }
+        }
+
+        if(ListadeArchivos.length<1)
+        {
+            NombresArchivos.add("No hay ningun archivo");
+            RutasArchivos.add(rutadirectorio);
+        }
+
+        Adaptador = new ArrayAdapter<String>(getActivity(),R.layout.fragment_huffman,NombresArchivos);
+        Lista.setAdapter(Adaptador);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+        File archivo = new File(RutasArchivos.get(i));
+        if(archivo.isFile())
+        {
+            Toast.makeText(getActivity(), "Has Seleccionado El Archivo: "+archivo.getName(),Toast.LENGTH_SHORT).show();
+        }
+        else
+        {
+            VerDirectorio(RutasArchivos.get(i));
+        }
+    }
 }
